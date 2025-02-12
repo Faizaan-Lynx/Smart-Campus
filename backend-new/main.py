aaa0bcd3c49700940ef0b15680a5f6555eb2c07c
FaizaanAlFaisal <101617018+FaizaanAlFaisal@users.noreply.github.com>
2025-02-12 11:43:14 +0500

    done: docker setup for dev

--- backend-new/main.py
+++ backend-new/main.py
@@ -1,1 +1,8 @@
-from api.alerts.routes import router
+from fastapi import FastAPI
+# from core.celery import worker, tasks
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
