ad84074491e8ad53bf63098c1c4d050d2d5f7212
Redgerd <msalaarm@gmai.com.com>
2025-02-17 08:54:57 +0500

    camera routes crated and tested

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,7 @@ from core.database import test_db_connection
 
 # Routers (uncomment when needed)
 from api.auth.routes import router as auth_router  
-# from api.cameras.routes import router as cameras_router
+from api.cameras.routes import router as cameras_router
 from api.alerts.routes import router as alerts_router
 # from api.intrusion.routes import router as intrusion_router
 
@@ -36,7 +36,7 @@ def startup_db_check():
 
 # Routes
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
-# app.include_router(cameras_router, prefix="/camera", tags=["cameras"])
+app.include_router(cameras_router, prefix="/camera", tags=["cameras"])
 app.include_router(alerts_router, prefix="/alerts", tags=["alerts"])
 # app.include_router(intrusion_router, prefix="/intrusion", tags=["intrusion"])
 
