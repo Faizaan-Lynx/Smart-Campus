86d9fed9211b3b0afc6f937c384c4e27af7737e3
FaizaanAlFaisal <101617018+FaizaanAlFaisal@users.noreply.github.com>
2025-03-06 11:38:41 +0500

    add: model worker startup function

--- backend/core/celery/model_worker.py
+++ backend/core/celery/model_worker.py
@@ -1,25 +1,33 @@
-# call this worker with the following command:
-# celery -A core.celery.worker worker --loglevel=info --queues=unprocessed_queue
-
-from celery import Celery
-from celery import task
+from celery import Celery, signals
 from config import settings
 import time
 from ultralytics import YOLO
+import numpy as np
+import logging
 
 model_worker_app = Celery('model_worker', broker=settings.REDIS_URL, backend=settings.REDIS_URL)
 
 # global model
 model = None
 
+
+@signals.worker_ready.connect
 def load_model():
     """
     Load the machine learning model into memory.
     """
+    try:    
+        global model
+        model = YOLO(model="./yolo-models/yolov8n.pt")
+        logging.info("Model loaded successfully.")
 
-    global model
-    model = YOLO(model="./yolo-models/yolov8n.pt")
-    print("Model loaded successfully.")
+        # run a dummy prediction to initialize the model
+        dummy_image = np.zeros((1280, 720, 3), dtype=np.uint8)
+        model.predict(dummy_image)
+        logging.info("Model initialized successfully.")    
+    except Exception as e:
+        logging.exception(e)
+        logging.error("Error loading model.")
 
 
 @model_worker_app.task
