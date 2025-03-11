b7630d139b80b3dbe57fba189e712fcfaf6faec2
FaizaanAlFaisal <101617018+FaizaanAlFaisal@users.noreply.github.com>
2025-03-11 14:25:57 +0500

    update: celery worker configs

--- backend/core/celery/worker.py
+++ backend/core/celery/worker.py
@@ -8,5 +8,11 @@ celery_app = Celery(
     backend=settings.REDIS_URL
 )
 
+celery_app.conf.update(
+    task_time_limit=30,
+    broker_transport_options={"visibility_timeout": 3600},
+    worker_heartbeat=60,
+)
+
 # Auto-discover tasks in alert_tasks and tasks
 celery_app.autodiscover_tasks(["core.celery.alert_tasks", "core.celery.tasks"])
