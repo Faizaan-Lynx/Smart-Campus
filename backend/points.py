284b7ec1f0ff42d7093cc75b4ade9537cc979307
FaizaanAlFaisal <101617018+FaizaanAlFaisal@users.noreply.github.com>
2025-05-12 12:25:13 +0500

    updated points tracker

--- backend/points.py
+++ backend/points.py
@@ -3,25 +3,37 @@ from dotenv import load_dotenv
 load_dotenv(override=True)
 import os
 
-cap = cv2.VideoCapture("rtsp://ncsael:Rawalians1234@172.23.10.180")
+cap = cv2.VideoCapture("data/sample2.mp4")
 if not cap.isOpened():
     print("Error: Could not open video.")
     exit()
 
+def mouse_click(event, x, y, flags, params):
+    global frame
+    if event == cv2.EVENT_LBUTTONDOWN:
+        frame = cv2.circle(frame, (x,y), 5, (0,0,255), -1)
+        points.append((x,y))
+        if len(points) >= 2:
+            frame = cv2.line(frame, points[-1], points[-2], (0,255,0), 2)
+        print(f"({x},{y})")
 
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        print("Error: Could not read frame.")
-        continue
+ret, frame = cap.read()
+print(frame.shape)
+if not ret:
+    print("Error: Could not read frame.")
+    exit()
 
-    resize_dims = eval(os.getenv("FEED_DIMS", "(640,480)"))
-    frame = cv2.resize(frame, resize_dims)
+# crop_dims = ((1280, 720), (640+1280, 720+480))
+# frame = frame[-960:, -1280:]
 
-    # displaying the image with cv2
-    cv2.imshow("Frame", frame)
+resize_dims = eval(os.getenv("FEED_DIMS", "(640,480)"))
+frame = cv2.resize(frame, resize_dims)
 
-    if cv2.waitKey(0):
-        break
-cv2.destroyAllWindows()
+# displaying the image with cv2
+cv2.imshow("Frame", frame)
+points = []
+cv2.setMouseCallback("Frame", mouse_click)
+
+if cv2.waitKey(0):
+    cv2.destroyAllWindows()
 cap.release()
